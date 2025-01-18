eef60906f673c7bd4bafc650f2c3746cc192de85
Tomasz Ostaszewski <ost.tomasz@gmail.com>
2025-01-18 16:41:55 +0100

    fixed controllers

--- models/controller.js
+++ models/controller.js
@@ -9,9 +9,10 @@
 
 import * as ContactsService from "./service.js";
 
-const idsList = async () =>
-  await ContactsService.getAll().map((contact) => contact.id);
-
+const idsList = async () => {
+  const contactList = await ContactsService.getAll();
+  return contactList.map((contact) => contact.id);
+};
 export const getAll = async (req, res) => {
   const contacts = await ContactsService.getAll();
   return res.status(200).json(contacts);
@@ -19,7 +20,7 @@ export const getAll = async (req, res) => {
 
 export const getById = async (req, res) => {
   const id = req.params.contactId;
-  const existingIds = idsList();
+  const existingIds = await idsList();
   if (!existingIds.includes(id)) {
     return res
       .status(404)
@@ -42,13 +43,13 @@ export const createContact = async (req, res) => {
 
 export const deleteContact = async (req, res) => {
   const id = req.params.contactId;
-  const existingIds = idsList();
+  const existingIds = await idsList();
   if (!existingIds.includes(id)) {
     return res
       .status(404)
       .json({ message: `Contact with id ${id} not found!` });
   }
-  ContactsService.deleteById(id);
+  await ContactsService.deleteById(id);
   return res.status(200).json({ message: `Contact with id ${id} deleted!` });
 };
 
@@ -58,20 +59,21 @@ export const updateContactById = async (req, res) => {
   if (Object.keys(body).length === 0) {
     return res.status(400).json({ message: "Missing fields" });
   }
-  const existingIds = idsList();
+  const existingIds = await idsList();
   if (!existingIds.includes(id)) {
     return res
       .status(404)
       .json({ message: `Contact with id ${id} not found!` });
   }
-  ContactsService.updateById(id, body);
+  await ContactsService.updateById(id, body);
   return res.status(200).json({ message: `updated contact with id ${id}` });
 };
 
 export const updateStatus = async (req, res) => {
-  const { favorite } = req.body;
+  const favorite = req.body;
+  console.log(favorite);
   const id = req.params.contactId;
-  const existingIds = idsList();
+  const existingIds = await idsList();
   if (!favorite) {
     return res.status(400).json({ message: "Missing field favorite" });
   }
@@ -81,6 +83,6 @@ export const updateStatus = async (req, res) => {
       .status(404)
       .json({ message: `Contact with id ${id} not found!` });
   }
-  ContactsService.updateStatusContact(id, favorite);
+  await ContactsService.updateStatusContact(id, favorite);
   return res.status(200).json({ message: `updated contact with id ${id}` });
 };
--- models/service.js
+++ models/service.js
@@ -3,7 +3,7 @@ export const getAll = () => Contact.find({});
 
 export const getById = (id) => Contact.findOne({ _id: id });
 
-export const create = (contact) => Contact.create({ contact });
+export const create = (contact) => Contact.create(contact);
 
 export const updateById = (id, updatedContact) =>
   Contact.findOneAndUpdate({ _id: id }, updatedContact);
@@ -11,4 +11,4 @@ export const updateById = (id, updatedContact) =>
 export const updateStatusContact = (id, favorite) =>
   Contact.findOneAndUpdate({ _id: id }, favorite);
 
-export const deleteById = (id) => Contact.findByIdAndDelete({ _id: id });
+export const deleteById = (id) => Contact.findByIdAndDelete(id);
