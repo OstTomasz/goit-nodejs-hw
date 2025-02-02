7e973e53dff51483185c0a63e6dc56887575aa1f
Tomasz Ostaszewski <ost.tomasz@gmail.com>
2025-02-02 15:34:37 +0100

    passport config

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,4 +1,28 @@
 import passport from "passport";
+import passportJWT from "passport-jwt";
+import { User } from "../models/users/repository.js";
+const secret = process.env.JWT_SECRET;
+
+const ExtractJWT = passportJWT.ExtractJwt;
+const Strategy = passportJWT.Strategy;
+const params = {
+  secretOrKey: secret,
+  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+};
+
+// JWT Strategy
+passport.use(
+  new Strategy(params, function (payload, done) {
+    User.find({ _id: payload.id })
+      .then(([user]) => {
+        if (!user) {
+          return done(new Error("User not found"));
+        }
+        return done(null, user);
+      })
+      .catch((err) => done(err));
+  })
+);
 
 export const auth = (req, res, next) => {
   passport.authenticate("jwt", { session: false }, (err, user) => {
